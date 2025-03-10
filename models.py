dac3245f23faa3b47e785ee91c980ef5a436d330
Mll0032 <mllawson18@gmail.com>
2025-03-09 22:28:16 -0500

    Update models to include magic items, gear, and tools This will update the dnd_items.db.

--- models.py
+++ models.py
@@ -24,3 +24,31 @@ class Armor(db.Model):
     weight = db.Column(db.Float)
     source = db.Column(db.String(100))
     homebrew = db.Column(db.Boolean, default=False)
+
+class MagicItem(db.Model):
+    __tablename__ = 'magic_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    rarity = db.Column(db.Enum('Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary', 'Artifact', name="rarity_enum"), nullable=False)
+    attunement = db.Column(db.Boolean, default=False, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(255))
+
+class Gear(db.Model):
+    __tablename__ = 'gear'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    cost = db.Column(db.Numeric(10,2), nullable=False)
+    weight = db.Column(db.Numeric(5,2), nullable=False)
+
+class Tool(db.Model):
+    __tablename__ = 'tools'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    cost = db.Column(db.Numeric(10,2), nullable=False)
+    weight = db.Column(db.Numeric(5,2), nullable=False)
